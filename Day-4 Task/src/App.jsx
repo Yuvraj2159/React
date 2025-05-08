d9db6bc02e890dcc79b250bdcfa9d4b67521b9de
Yuvraj2159 <132037227+Yuvraj2159@users.noreply.github.com>
2025-05-08 20:38:42 +0545

    Day-4

---
+++ Day-4 Task/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+function AnotherPage() {
+  
+  return (
+    <div className='p-4 flex flex-col justify-center items-center'>
+      <Button title={"Add"}/>
+      <br />
+      <br />
+      <br />
+      <Button title={"submit"} />
+    </div>
+  )
+}
+
+export default AnotherPage
---
+++ Day-4 Task/src/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ title }) => {
+  return (
+    <div className="p-5 bg-slate-500 w-20 rounded-md text-white">
+      <button >{title}</button>
+    </div>
+  );
+};
+
+export default Button;
